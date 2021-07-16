9a8dbdd37a56b18b51c943af044f69cd6ff0d602
Andre Moresco <amoresco1@gmail.com>
2021-07-16 18:37:05 -0300

    Added project

---
+++ src/test/java/com/axcient/gsuitesafe/gmailbackup/GmailBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axcient.gsuitesafe.gmailbackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailBackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
